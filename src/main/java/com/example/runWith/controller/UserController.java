4fae1af079c648f012848d2bf0e5173f2472406b
seonyoeng <kseonyoeng@naver.com>
2022-01-22 19:44:37 +0900

    includeStacktrace

--- src/main/java/com/example/runWith/controller/UserController.java
+++ src/main/java/com/example/runWith/controller/UserController.java
@@ -9,7 +9,7 @@ import java.util.HashMap;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("user")
 public class UserController {
     @Autowired
     UserService userService;
