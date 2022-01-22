f2cb6664980b465e1e50f1e46ddc16d5e830da31
seonyoeng <kseonyoeng@naver.com>
2022-01-22 19:15:54 +0900

    record

--- src/main/java/com/example/runWith/controller/RecordController.java
+++ src/main/java/com/example/runWith/controller/RecordController.java
@@ -6,10 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/record")
@@ -17,20 +13,11 @@ public class RecordController {
     @Autowired
     private RecordService recordService;
 
-    @GetMapping
-    public List<RecordDomain> findAllRecord() {
-        return recordService.findAllRecord();
-    }
-
-
-    @GetMapping("/findRecord")
+    @GetMapping("/find")
     public RecordDomain findRecord(@RequestParam("id") String id, @RequestParam("day") String day) {
         return recordService.findRecord(id, day);
     }
 
-    @PostMapping("/addRecord")
+    @PostMapping("/add")
     public void addRecord(@RequestBody RecordDomain newRecord) { recordService.addRecord(newRecord);}
-
-    @GetMapping("/showRecord")
-    public List<RecordDomain> showRecord(@RequestParam("id") String id) { return recordService.showRecord(id); }
 }
--- src/main/java/com/example/runWith/mapper/RecordMapper.java
+++ src/main/java/com/example/runWith/mapper/RecordMapper.java
@@ -8,9 +8,7 @@ import java.util.List;
 
 @Mapper
 public interface RecordMapper {
-    public List<RecordDomain> findAllRecord();
     public RecordDomain findRecord(@Param("id") String id, @Param("day") String day);
     public void addRecord(@Param("newRecord") RecordDomain newRecord);
     public void updateRecord(@Param("newRecord") RecordDomain newRecord);
-    public List<RecordDomain> showRecord(@Param("id") String id);
 }
