ccaa6ec80b869dafde99b039439ba47f1f8a9c17
seonyoeng <kseonyoeng@naver.com>
2022-01-22 20:07:20 +0900

    ServerApplication

--- src/main/java/com/example/runWith/RunWithServerApplication.java
+++ src/main/java/com/example/runWith/RunWithServerApplication.java
@@ -1,12 +1,10 @@
 package com.example.runWith;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @ComponentScan
-@MapperScan
 @SpringBootApplication
 public class RunWithServerApplication {
 	public static void main(String[] args) {
