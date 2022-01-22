121e93d7c56dfd6aa4a9d28f659be810125debd0
seonyoeng <kseonyoeng@naver.com>
2022-01-22 20:11:20 +0900

    ServerApplication

--- src/main/java/com/example/runWith/RunWithServerApplication.java
+++ src/main/java/com/example/runWith/RunWithServerApplication.java
@@ -2,11 +2,10 @@ package com.example.runWith;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan
 @SpringBootApplication
 public class RunWithServerApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(RunWithServerApplication.class, args);
 	}
